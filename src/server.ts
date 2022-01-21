d71e0df71a6f3478c9ec36ff72bf2418fbebbe0a
dtmiranda <hortadito2@gmail.com>
2022-01-21 17:46:06 -0100

    eslint, editorconfig and prettier integration

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express, { request } from 'express';
+import routes from './routes';
 
-const app =express();
+const app = express();
 
-app.get('/', (request, response) =>{
-    return response.json({ message: 'Hello Cabo vErde'});
-})
+app.use(routes);
 
 app.listen(3333, () =>{
     console.log('Server started on port 3333');
